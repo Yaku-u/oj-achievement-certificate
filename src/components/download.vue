fe908788683fcd571b1678bce76a660030b4ed00
Yaku-u <1816140905@qq.com>
2025-08-14 22:13:27 +0800

    upd: 添加主页样式

---
+++ src/components/download.vue
@@ -0,0 +1,118 @@
+<template>
+    <div class="downMenu">
+        <div class="downBtn" @click="print">
+            <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px">
+                <path
+                    d="M640-640v-120H320v120h-80v-200h480v200h-80Zm-480 80h640-640Zm560 100q17 0 28.5-11.5T760-500q0-17-11.5-28.5T720-540q-17 0-28.5 11.5T680-500q0 17 11.5 28.5T720-460Zm-80 260v-160H320v160h320Zm80 80H240v-160H80v-240q0-51 35-85.5t85-34.5h560q51 0 85.5 34.5T880-520v240H720v160Zm80-240v-160q0-17-11.5-28.5T760-560H200q-17 0-28.5 11.5T160-520v160h80v-80h480v80h80Z" />
+            </svg>
+            Print
+        </div>
+        <div class="downBtn" @click="downloadAsPNG">
+            <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px">
+                <path
+                    d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm40-80h480L570-480 450-320l-90-120-120 160Zm-40 80v-560 560Z" />
+            </svg>
+            Export as PNG
+        </div>
+        <div class="downBtn" @click="downloadAsPDF">
+            <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px">
+                <path
+                    d="M360-460h40v-80h40q17 0 28.5-11.5T480-580v-40q0-17-11.5-28.5T440-660h-80v200Zm40-120v-40h40v40h-40Zm120 120h80q17 0 28.5-11.5T640-500v-120q0-17-11.5-28.5T600-660h-80v200Zm40-40v-120h40v120h-40Zm120 40h40v-80h40v-40h-40v-40h40v-40h-80v200ZM320-240q-33 0-56.5-23.5T240-320v-480q0-33 23.5-56.5T320-880h480q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H320Zm0-80h480v-480H320v480ZM160-80q-33 0-56.5-23.5T80-160v-560h80v560h560v80H160Zm160-720v480-480Z" />
+            </svg>
+            Export as PDF
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import html2canvas from "html2canvas";
+    import { jsPDF } from "jspdf";
+    import { toRefs } from "vue";
+
+    const props = defineProps({
+        certificateRef: Object
+    });
+
+    const { certificateRef } = toRefs(props);
+
+    async function captureCertificate() {
+        const target = certificateRef.value?.$el || certificateRef.value;
+        if (!target) return;
+
+        return await html2canvas(target, {
+            scale: 2,
+            useCORS: true,
+            backgroundColor: "#ffffff"
+        });
+    }
+
+    async function downloadAsPNG() {
+        const canvas = await captureCertificate();
+        if (!canvas) return;
+        const link = document.createElement("a");
+        link.href = canvas.toDataURL("image/png");
+        link.download = "certificate.png";
+        link.click();
+    }
+
+    async function downloadAsPDF() {
+        const canvas = await captureCertificate();
+        if (!canvas) return;
+        const imgData = canvas.toDataURL("image/png");
+
+        const pdf = new jsPDF("p", "mm", "a4");
+        const pageWidth = pdf.internal.pageSize.getWidth();
+        const pageHeight = pdf.internal.pageSize.getHeight();
+        const imgWidth = pageWidth;
+        const imgHeight = (canvas.height * imgWidth) / canvas.width;
+
+        if (imgHeight <= pageHeight) {
+            pdf.addImage(imgData, "PNG", 0, 0, imgWidth, imgHeight);
+        } else {
+            let heightLeft = imgHeight;
+            let position = 0;
+            pdf.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
+            heightLeft -= pageHeight;
+
+            while (heightLeft > 0) {
+                position = heightLeft - imgHeight;
+                pdf.addPage();
+                pdf.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
+                heightLeft -= pageHeight;
+            }
+        }
+
+        pdf.save("certificate.pdf");
+    }
+</script>
+
+<style scoped>
+    .downMenu {
+        transition: all 0.3s ease;
+        position: absolute;
+        top: 60px;
+        right: 20px;
+        background-color: white;
+        border: 1px solid #ccc;
+        min-width: 100px;
+        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.15);
+        border-radius: 4px;
+        z-index: 100;
+    }
+
+    .downBtn {
+        padding: 15px;
+        cursor: pointer;
+        transition: all 0.3s;
+        font-size: 14px;
+        display: flex;
+        align-items: center;
+        gap: 4px;
+    }
+
+    .downBtn:hover {
+        color: #40a9ff;
+        fill: #40a9ff;
+    }
+
+</style>
---
+++ src/components/menu.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="menu">
+        <div class="menu-header">
+            <div class="home-button">
+                <a href="https://oj.sdutacm.cn/">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="50px" viewBox="0 -960 960 960" width="26px">
+                        <path
+                            d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z" />
+                    </svg>
+                </a>
+            </div>
+            <div class="title">
+                SDUT OJ Achievement Certificate
+            </div>
+            <div class="menu-button">
+                <div class="github">
+                    <svg width="23px" height="50px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 98 97">
+                        <path
+                            d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z" />
+                    </svg>
+                </div>
+                <div class="download" ref="menuWrapper" @click="showDownloadModal = !showDownloadModal">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="50px" viewBox="0 -960 960 960" width="25px">
+                        <path
+                            d="M240-40q-33 0-56.5-23.5T160-120v-440q0-33 23.5-56.5T240-640h120v80H240v440h480v-440H600v-80h120q33 0 56.5 23.5T800-560v440q0 33-23.5 56.5T720-40H240Zm200-280v-447l-64 64-56-57 160-160 160 160-56 57-64-64v447h-80Z" />
+                    </svg>
+                    <div name="dropdown">
+                        <print v-if="showDownloadModal" :certificateRef="certificateRef"></print>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import { ref, onMounted, onBeforeUnmount } from 'vue'
+    import print from "@/components/download.vue"
+
+    const showDownloadModal = ref(false);
+    const menuWrapper = ref(null);
+    const props = defineProps({
+        certificateRef: Object
+    });
+
+    function handleClickOutside(e) {
+        if (menuWrapper.value && !menuWrapper.value.contains(e.target)) {
+            showDownloadModal.value = false
+        }
+    }
+
+    onMounted(() => {
+        document.addEventListener('click', handleClickOutside)
+    })
+
+    onBeforeUnmount(() => {
+        document.removeEventListener('click', handleClickOutside)
+    })
+
+</script>
+
+<style scoped>
+    @import url(../assets/menu.css);
+</style>
